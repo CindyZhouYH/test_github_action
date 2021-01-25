3796a3b8bc53f0292e809b8b5f2c1cf31e2fdb93
周远航 <18373004@buaa.edu.cn>
2021-01-25 23:09:04 +0800

    fix empty spaces

--- test/api/execute/test_execute.py
+++ test/api/execute/test_execute.py
@@ -18,15 +18,14 @@ class TestException:
     def test_execute_dcmodule_1(self):
         with open(testfile_dir, "w+") as fp:
             fp.write("""
-                    from dcmodule import load_with_args, result_dump
-                    if __name__ == "__main__":
-                        with load_with_args() as _iotuple:
-                            _stdin, _stdout = _iotuple
-                            result_dump(True, data={
-                                "stdin": _stdin,
-                                "stdout": _stdout,
-                            })
-
+from dcmodule import load_with_args, result_dump
+if __name__ == "__main__":
+    with load_with_args() as _iotuple:
+        _stdin, _stdout = _iotuple
+        result_dump(True, data={
+            "stdin": _stdin,
+            "stdout": _stdout,
+        })
                      """)
             fp.close()
         with open(input_dir, "w+") as fp:
@@ -50,16 +49,15 @@ class TestException:
     def test_execute_dcmodule_2(self):
         with open(testfile_dir, "w+") as fp:
             fp.write("""
-                                from dcmodule import load_with_args, result_dump
-                                if __name__ == "__main__":
-                                    with load_with_args() as _iotuple:
-                                        _stdin, _stdout = _iotuple
-                                        result_dump(True, data={
-                                            "stdin": _stdin,
-                                            "stdout": _stdout,
-                                        })
-
-                                 """)
+from dcmodule import load_with_args, result_dump
+if __name__ == "__main__":
+    with load_with_args() as _iotuple:
+        _stdin, _stdout = _iotuple
+        result_dump(True, data={
+            "stdin": _stdin,
+            "stdout": _stdout,
+        })
+                     """)
             fp.close()
         with open(input_dir, "w+") as fp:
             fp.write("1 2 3")
